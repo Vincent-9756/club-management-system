c7a285ca161ce9b939a5fde1eb228c3e37ffc7ef
wuxinyu-1649963917 <1649963917@qq.com>
2019-12-06 15:54:57 +0800

    111

--- js/admin/personal-management.js
+++ js/admin/personal-management.js
@@ -21,11 +21,11 @@ $.ajax({
       }
     }
     $('.community-nav').append(data);
-    
+
     $('.community-nav').on('click', '.community-item a', function () {
       $(this).addClass('community-this').parent().siblings().children().removeClass('community-this');
     });
-    
+
     $('.community-item').eq(0).children('a').click();
   }
 });
