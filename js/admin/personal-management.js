4c79ae65fbbf687f43d5a2dd6216bb2471f68bda
SoMessThat <842387850@qq.com>
2020-01-06 17:38:33 +0800

    1

--- js/admin/personal-management.js
+++ js/admin/personal-management.js
@@ -5,7 +5,7 @@ $.ajax({
   dataType: "json",
   contentType: "application/json;charset=UTF-8",
   data: JSON.stringify({
-    "name": ''
+    "username": ''
   }),
   success: function (res) {
     var data = '';
@@ -26,23 +26,23 @@ $.ajax({
     //点击刷新干事列表
     $('.community-nav').on('click', '.community-item a', function () {
       $(this).addClass('community-this').parent().siblings().children().removeClass('community-this');
-
+      $("#name").val("");
       var departmentId = $(this).attr("value");
-      refresh(departmentId);
+      refresh(departmentId,"");
     });
 
     $('.community-item').eq(0).children('a').click();
   }
-  
+
 });
 
-function refresh(id) {
+function refresh(id,name) {
   $('#EmployeeDemo').html("");
-  layui.use('flow', function(){
+  layui.use('flow', function () {
     var flow = layui.flow;
     flow.load({
       elem: '#EmployeeDemo',
-      done: function(page, next){
+      done: function (page, next) {
         var lis = [];
         $.ajax({
           type: "post",
@@ -50,42 +50,42 @@ function refresh(id) {
           dataType: "json",
           contentType: "application/json;charset=UTF-8",
           data: JSON.stringify({
-            "name": '',
+            "username": name,
             "departmentId": id,
           }),
           success: function (res) {
-            layui.each(res.data, function(index, item){
+            layui.each(res.data, function (index, item) {
               lis.push('<div class="layui-col-md4 layui-col-sm6">');
               lis.push('<div class="layadmin-contact-box" > ');
               lis.push('<div class="layui-col-md4 layui-col-sm6">');
               lis.push('<div class="layadmin-text-center">');
-              lis.push('<img src="'+item.avatar+'">');
+              lis.push('<img src="' + item.avatar + '">');
               lis.push('</div>');
               lis.push('</div>');
               lis.push('<div class="layui-col-md8 layadmin-padding-left20 layui-col-sm6">');
               lis.push('<div class="layui-row">');
               lis.push('<div class="layui-col-md8">');
               lis.push('<h3 class="layadmin-title">');
-              lis.push('<strong>'+item.username+'</strong>');
+              lis.push('<strong>' + item.username + '</strong>');
               lis.push('</h3>');
               lis.push('</div>');
               lis.push('</div>');
               lis.push('<div class="layadmin-address">');
-              lis.push('<p>'+item.account+'<p>');
-              lis.push('<p><i class="layui-icon layui-icon-cellphone"></i>:'+item.tel+'</p>');
+              lis.push('<p>' + item.account + '<p>');
+              lis.push('<p><i class="layui-icon layui-icon-cellphone"></i>:' + item.tel + '</p>');
               lis.push('</div>');
               lis.push('</div>');
               lis.push('<div class="layadmin-maillist-img layadmin-font-blod">');
               lis.push('<div class="layui-btn-group" style="margin-top: 12px">');
-              lis.push('<button class="layui-btn layui-btn-primary layui-btn-sm dismiss" value="'+ item.id +'">辞退</button>');
-              lis.push('<button class="layui-btn layui-btn-primary layui-btn-sm appoint1" value="'+ item.id +'">任命为社长</button>');
-              lis.push('<button class="layui-btn layui-btn-primary layui-btn-sm appoint2" value="'+ item.id +'">任命为副社长</button>');
+              lis.push('<button class="layui-btn layui-btn-primary layui-btn-sm dismiss" value="' + item.id + '">辞退</button>');
+              lis.push('<button class="layui-btn layui-btn-primary layui-btn-sm appoint1" value="' + item.id + '">任命为社长</button>');
+              lis.push('<button class="layui-btn layui-btn-primary layui-btn-sm appoint2" value="' + item.id + '">任命为副社长</button>');
               lis.push('</div>');
               lis.push('</div>');
               lis.push('</div>');
               lis.push('</div>');
-            }); 
-            next(lis.join(' '), page < res.count/6);    
+            });
+            next(lis.join(' '), page < res.count / 6);
           }
         });
       }
@@ -93,8 +93,58 @@ function refresh(id) {
   });
 }
 
+//辞退
 $('body').on('click', '.dismiss', function () {
-  //辞退
-  refresh($(this).attr("value"));
-  // $(this).remove();
+  $.ajax({
+    type: "get",
+    url: url + "/user/dismiss",
+    dataType: "json",
+    contentType: "application/json;charset=UTF-8",
+    data: {
+      "id": $(this).attr("value")
+    },
+    success: function (res) {
+      parent.layui.layer.msg(res.msg);
+      refresh($(".community-this").attr("value"),"");
+    }
+  });
+});
+
+//任命社长
+$('body').on('click', '.appoint1', function () {
+  $.ajax({
+    type: "get",
+    url: url + "/user/appoint1",
+    dataType: "json",
+    contentType: "application/json;charset=UTF-8",
+    data: {
+      "id": $(this).attr("value")
+    },
+    success: function (res) {
+      parent.layui.layer.msg(res.msg);
+      refresh($(".community-this").attr("value"),"");
+    }
+  });
+});
+
+//任命副社长
+$('body').on('click', '.appoint2', function () {
+  $.ajax({
+    type: "get",
+    url: url + "/user/appoint2",
+    dataType: "json",
+    contentType: "application/json;charset=UTF-8",
+    data: {
+      "id": $(this).attr("value")
+    },
+    success: function (res) {
+      parent.layui.layer.msg(res.msg);
+      refresh($(".community-this").attr("value"),"");
+    }
+  });
+});
+
+$('#name').on('input propertychange', function () {
+  name = $('#name').val();
+  refresh($(".community-this").attr("value"), $('#name').val());
 });
